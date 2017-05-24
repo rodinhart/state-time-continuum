7cc05328a885336d327c7f0b8d4c4a4509d9f804
Rodin van der Hart <rodin.hart@concentra.co.uk>
2017-05-24 19:13:57 +0100

    Fixed type

--- src/Cause.js
+++ src/Cause.js
@@ -25,7 +25,7 @@ const app = render => reduce => effect => {
     const newEffect = reduce(cause)(effect.state)
     var io = newEffect.io
 
-    io = io ? render(newEffect.state).bin(() => io) : render(newEffect.state)
+    io = io ? render(newEffect.state).bind(() => io) : render(newEffect.state)
 
     return Effect(newEffect.state, io)
   }).bind(loop)
