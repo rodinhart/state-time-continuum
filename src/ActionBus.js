f861ab1f9da41203562c4a0873666f30e3e5f811
Rodin van der Hart <rodin.hart@concentra.co.uk>
2017-09-13 16:15:17 +0100

    Added dispatch when running reaction

--- src/ActionBus.js
+++ src/ActionBus.js
@@ -3,27 +3,30 @@ const Task = require("data-control").Task
 module.exports = () => {
   let _
 
+  // dispatch :: Action -> ()
+  const dispatch = action => {
+    if (!_)
+      throw new Error(
+        "Dispatch no longer set, nobody is listening, action lost: " +
+        action.type
+      )
+    _(action)
+  }
+
+  // listen :: Effect Action a -> Task () Action
+  const listen = effect =>
+    Task((rej, res) => {
+      if (_) throw new Error("Dispatch already set, someone is listening...")
+      _ = action => {
+        _ = undefined
+        res(action)
+      }
+
+      if (effect.reaction) effect.reaction.run(effect.state, dispatch)
+    })
+
   return {
-    // dispatch :: Action -> ()
-    dispatch: action => {
-      if (!_)
-        throw new Error(
-          "Dispatch no longer set, nobody is listening, action lost: " +
-            action.type
-        )
-      _(action)
-    },
-
-    // listen :: Effect Action a -> Task () Action
-    listen: effect =>
-      Task((rej, res) => {
-        if (_) throw new Error("Dispatch already set, someone is listening...")
-        _ = action => {
-          _ = undefined
-          res(action)
-        }
-
-        if (effect.reaction) effect.reaction.run(effect.state)
-      })
+    dispatch,
+    listen
   }
 }
